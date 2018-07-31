21dcdfbdf2015df1a32b12f21178bcec2af8b14c
skjanani <41111202+skjanani@users.noreply.github.com>
2018-07-31 14:50:02 +0530

    Add files via upload

---
+++ Nextnum.java
@@ -0,0 +1,15 @@
+package janani;
+import java.util.Scanner;
+public class Nextnum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int n,sum=0;
+System.out.println("enter the number");
+Scanner ja=new Scanner(System.in);
+n=ja.nextInt();
+sum=n+1;
+System.out.println(sum);
+	}
+
+}
